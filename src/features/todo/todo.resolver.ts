dfa96e7e7bce6685dabeb59261be9a58ecebb14b
Injector1 <injector005@gmail.com>
2021-08-12 18:20:31 +0500

    feat: add todo input
    * added todo graphql input type

--- src/features/todo/todo.resolver.ts
+++ src/features/todo/todo.resolver.ts
@@ -1,10 +1,31 @@
 import { UseGuards } from '@nestjs/common';
-import { Resolver, Query, Mutation, Args, Int, ID } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  Int,
+  ID,
+  InputType,
+  Field,
+} from '@nestjs/graphql';
 import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
 import { Todo } from './todo.model';
 import { TodoService } from './todo.service';
 import { UserEntity } from '../user/user.decorator';
 import { User } from '@prisma/client';
+
+@InputType()
+class TodoInput extends Todo {
+  @Field(() => ID, { nullable: true })
+  id: string;
+
+  @Field(() => String, { defaultValue: '' })
+  title: string;
+
+  @Field(() => Boolean, { defaultValue: false })
+  isDone: boolean;
+}
 
 @Resolver(() => Todo)
 export class TodoResolver {
@@ -20,20 +41,23 @@ export class TodoResolver {
   @Mutation(() => Todo)
   createTodo(
     @UserEntity() user: User,
-    @Args('title', { type: () => String }) title: string,
-    @Args('isDone', { type: () => Boolean }) isDone: boolean,
+    @Args('todoInput') todoInput: TodoInput,
   ) {
-    return this.todoService.createTodo(title, isDone, user.id);
+    return this.todoService.createTodo(
+      todoInput.title,
+      todoInput.isDone,
+      user.id,
+    );
   }
 
   @UseGuards(GqlAuthGuard)
   @Mutation(() => Todo)
-  updateTodo(
-    @Args('id', { type: () => ID }) id: string,
-    @Args('title', { defaultValue: '' }) title: string,
-    @Args('isDone', { defaultValue: false }) isDone: boolean,
-  ) {
-    return this.todoService.changeTodo(id, title, isDone);
+  updateTodo(@Args('updateTodoInput') updateTodoInput: TodoInput) {
+    return this.todoService.changeTodo(
+      updateTodoInput.id,
+      updateTodoInput.title,
+      updateTodoInput.isDone,
+    );
   }
 
   @UseGuards(GqlAuthGuard)
